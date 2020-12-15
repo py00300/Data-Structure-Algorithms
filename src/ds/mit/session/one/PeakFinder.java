9cdfe00dd3745f14de2f06ad01afaaa4961c87d6
Ali MOLLA MOHAMMADI <alimolla.it@gmail.com>
2020-12-15 16:23:10 -0500

    code for peak finder in one diementional array added.

---
+++ src/ds/mit/session/one/PeakFinder.java
@@ -0,0 +1,26 @@
+package ds.mit.session.one;
+
+import java.util.Arrays;
+
+public class PeakFinder {
+
+  public static void main(String[] args) {
+    int[] numbers = {1,2,3,1};
+    int peak = findPeak(numbers);
+    System.out.println(peak);
+
+    int[] numbers2 = {1,2,1,3,5,6,4};
+    int peak2 = findPeak(numbers2);
+    System.out.println(peak2);
+  }
+
+  private static int findPeak(int[] numbers) {
+    int n = numbers.length;
+    if (numbers[n/2] < numbers[(n/2) - 1]) {
+       return findPeak(Arrays.copyOfRange(numbers, 0, (n/2) - 1));
+    } else if (numbers[n/2] < numbers[(n/2) + 1]) {
+      return findPeak(Arrays.copyOfRange(numbers, n/2, n));
+    }
+    return numbers[n/2];
+  }
+}
