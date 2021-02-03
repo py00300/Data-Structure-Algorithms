7113ca2ea0aeca812a315ea16d5335f54341ae77
Ali MOLLA MOHAMMADI <alimolla.it@gmail.com>
2021-02-03 08:50:39 -0500

    OOP concepts added, interview added

--- src/ds/mit/session/one/oracle/A.java
+++ src/ds/mit/session/one/oracle/A.java
@@ -34,4 +34,22 @@ public class A {
   19. concurrency design best practices and considerations?
   20. how find a loop in link list?
   21. check exception vs unchecked exception? why make exceptions checked or unchecked?
+  1). Difference between ArrayList and LinkedList?
+  2). What  is Map?
+  3). What is Vector?
+  4). Difference between Overloading and Overriding?
+  5). Can possible to create Object for Parent class with child class reference? ex: Class A{ //Statements } Class B extends A{ //Statements PSVM(){ B Reference_Variable=new A(); } }
+  6). What is Synchronization?
+  7). what is the time complexity of Arraylist and Linked List?
+  8). Write a program to print the higher version of a Value? Ex: Value1=7.3.4.2.5.9 Value2=7.3.2.3.8.1 o/p: 7.3.4.2.5.9 in these 2 values write a program to print the Higher Version?
+  9). Test case: "Hai i am SAP" O/P: "iaH i ma PAS" write a program in java for above O/P?
+  10). About Project?
+  11). Flow of execution of a project with Architecture?
+  12).Basic SQL Questions? (DDL DML TCl Commands)
+  13). About Agile/Driven Methodology?
+  14). What is final keyword?
+  15) what is Data hiding?
+  16) Design patterns? (Singleton Design Pattern program)
+  17). Internal implementation of HashSet?
+
  */
